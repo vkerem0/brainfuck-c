d8b07eca0dc488a2211cacb2c46de779f6350e64
Kerem Öztürk <69808366+vkerem0@users.noreply.github.com>
2025-07-09 20:15:21 +0300

    fixed potential pointer overflow/underflow

--- brainfuck.c
+++ brainfuck.c
@@ -1,27 +1,6 @@
-/*
- Brainfuck-C ( http://github.com/kgabis/brainfuck-c )
- Copyright (c) 2012 Krzysztof Gabis
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
-*/
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define OP_END          0
 #define OP_INC_DP       1
@@ -36,9 +15,9 @@
 #define SUCCESS         0
 #define FAILURE         1
 
-#define PROGRAM_SIZE    4096
-#define STACK_SIZE      512
-#define DATA_SIZE       65535
+#define PROGRAM_SIZE    65536
+#define STACK_SIZE      8192
+#define DATA_SIZE       65536
 
 #define STACK_PUSH(A)   (STACK[SP++] = A)
 #define STACK_POP()     (STACK[--SP])
@@ -55,6 +34,7 @@ static unsigned short STACK[STACK_SIZE];
 static unsigned int SP = 0;
 
 int compile_bf(FILE* fp) {
+    memset(PROGRAM, 0, sizeof(PROGRAM));
     unsigned short pc = 0, jmp_pc;
     int c;
     while ((c = getc(fp)) != EOF && pc < PROGRAM_SIZE) {
@@ -67,21 +47,18 @@ int compile_bf(FILE* fp) {
             case ',': PROGRAM[pc].operator = OP_IN; break;
             case '[':
                 PROGRAM[pc].operator = OP_JMP_FWD;
-                if (STACK_FULL()) {
-                    return FAILURE;
-                }
+                if (STACK_FULL()) return FAILURE;
                 STACK_PUSH(pc);
                 break;
             case ']':
-                if (STACK_EMPTY()) {
-                    return FAILURE;
-                }
+                if (STACK_EMPTY()) return FAILURE;
                 jmp_pc = STACK_POP();
-                PROGRAM[pc].operator =  OP_JMP_BCK;
+                PROGRAM[pc].operator = OP_JMP_BCK;
                 PROGRAM[pc].operand = jmp_pc;
                 PROGRAM[jmp_pc].operand = pc;
                 break;
-            default: pc--; break;
+            default:
+                continue; // Yorum dışı karakteri atla
         }
         pc++;
     }
@@ -93,41 +70,64 @@ int compile_bf(FILE* fp) {
 }
 
 int execute_bf() {
-    unsigned short data[DATA_SIZE] = {0}, pc = 0;
+    unsigned char data[DATA_SIZE] = {0};
+    unsigned short pc = 0;
     unsigned int ptr = 0;
 
-    while (PROGRAM[pc].operator != OP_END && ptr < DATA_SIZE) {
+    while (PROGRAM[pc].operator != OP_END) {
         switch (PROGRAM[pc].operator) {
-            case OP_INC_DP: ptr++; break;
-            case OP_DEC_DP: ptr--; break;
-            case OP_INC_VAL: data[ptr]++; break;
-            case OP_DEC_VAL: data[ptr]--; break;
-            case OP_OUT: putchar(data[ptr]); break;
-            case OP_IN: data[ptr] = (unsigned int)getchar(); break;
-            case OP_JMP_FWD: if(!data[ptr]) { pc = PROGRAM[pc].operand; } break;
-            case OP_JMP_BCK: if(data[ptr]) { pc = PROGRAM[pc].operand; } break;
-            default: return FAILURE;
+            case OP_INC_DP:
+                if (ptr + 1 >= DATA_SIZE) return FAILURE;
+                ptr++;
+                break;
+            case OP_DEC_DP:
+                if (ptr == 0) return FAILURE;
+                ptr--;
+                break;
+            case OP_INC_VAL:
+                data[ptr]++;
+                break;
+            case OP_DEC_VAL:
+                data[ptr]--;
+                break;
+            case OP_OUT:
+                putchar(data[ptr]);
+                break;
+            case OP_IN:
+                data[ptr] = (unsigned char)getchar();
+                break;
+            case OP_JMP_FWD:
+                if (!data[ptr]) pc = PROGRAM[pc].operand;
+                break;
+            case OP_JMP_BCK:
+                if (data[ptr]) pc = PROGRAM[pc].operand;
+                break;
+            default:
+                return FAILURE;
         }
         pc++;
     }
-    return ptr != DATA_SIZE ? SUCCESS : FAILURE;
+    return SUCCESS;
 }
 
-int main(int argc, const char * argv[])
-{
+int main(int argc, const char * argv[]) {
     int status;
     FILE *fp;
     if (argc != 2 || (fp = fopen(argv[1], "r")) == NULL) {
-        fprintf(stderr, "Usage: %s filename\n", argv[0]);
+        fprintf(stderr, "Usage: %s filename.bf\n", argv[0]);
         return FAILURE;
     }
+
     status = compile_bf(fp);
     fclose(fp);
+
     if (status == SUCCESS) {
         status = execute_bf();
     }
+
     if (status == FAILURE) {
         fprintf(stderr, "Error!\n");
     }
+
     return status;
 }
